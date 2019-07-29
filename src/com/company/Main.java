dc95217b306d3fb468b14d18872d944b5dc1a61f
vcmsss <pak.al.ig@yandex.ru>
2019-07-29 20:57:25 +0300

    Исправление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,113 +1,31 @@
 package com.company;
 
-
-
-
-
-
-
 import java.util.Scanner;
-
-
-
-
-
-
 
 public class Main {
 
 
-
-
-
-
-
     public static void main(String args[]){
 
-
-
-
-
-
-
         Scanner in = new Scanner(System.in);
-
-
-
         System.out.print("Text: ");
-
-
-
         String a = in.nextLine();
-
-
-
         int x = a.length();
 
-
-
         System.out.print("Text 2: ");
-
-
-
         String b = in.nextLine();
-
-
-
         int y = b.length();
 
 
-
-
-
-
-
-
-
-
-
         if(x>y)
-
-
-
             System.out.print(a);
 
-
-
-
-
-
-
         else if(x<y)
-
-
-
             System.out.print(b);
 
-
-
-
-
-
-
         else
-
-
-
             System.out.println("Строки равны");
 
-
-
-
-
-
-
     }
-
-
-
-
-
-
 
 }
